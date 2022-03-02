f7055165cbd31db2cfd826ce4c0659fa6ad336dd
Samira Awad <engineersamiraawad@gmail.com>
2022-03-02 19:04:37 +0300

    update instance

--- src/redux/actions/instance.js
+++ src/redux/actions/instance.js
@@ -3,7 +3,7 @@ axios.defaults.xsrfCookieName = "csrftoekn";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.withCredentials = true;
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8001/",
+  baseURL: "https://orable-driver-be.herokuapp.com/",
   withCredentials: true,
   // headers: {
   //   // "Access-Control-Allow-Origin": "http://127.0.0.1:8001",
